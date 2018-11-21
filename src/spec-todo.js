77bf73871c4dada47a7066099feae2c700b1bf04
Jon <jmkeiste@live.com>
2018-11-20 23:45:23 -0500

    Updated spec file

--- src/spec-todo.js
+++ src/spec-todo.js
@@ -5,13 +5,9 @@
 //first parameter - test case name
 //second parameter - function (function will have all test steps)
 
-describe('Protractor baby steps', function(){
-  it('Open AngularJS website', function(){
+describe('Protractor Demo App', function() {
+  it('Open AngularJS website', function() {
     //Protractor code goes here
-    
-    };
-  })
-  it('Close the browser', function(){
-    //Protractor code goes here
-  })
-})
+    browser.get('https://angularjs.org');
+  });
+});
