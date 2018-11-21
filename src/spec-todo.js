35854a1de1d7de16be4fb2301defdb7fe45a6bbf
Jon <jmkeiste@live.com>
2018-11-20 22:30:32 -0500

    Adding configuration and spec files

---
+++ src/spec-todo.js
@@ -0,0 +1,17 @@
+//describe:
+//first parameter - test suite name
+//second parameter - function (function will have all tests [it blocks])
+//it:
+//first parameter - test case name
+//second parameter - function (function will have all test steps)
+
+describe('Protractor baby steps', function(){
+  it('Open AngularJS website', function(){
+    //Protractor code goes here
+    
+    };
+  })
+  it('Close the browser', function(){
+    //Protractor code goes here
+  })
+})
