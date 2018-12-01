f397b24be27b19180eca8f9d42d2fa3e4b575734
Jon <jmkeiste@live.com>
2018-12-01 10:34:40 -0500

    Updated spec file to handle async function

--- src/spec-todo.js
+++ src/spec-todo.js
@@ -9,5 +9,8 @@ describe('Protractor Demo App', function() {
   it('Open AngularJS website', function() {
     //Protractor code goes here
     browser.get('https://angularjs.org');
+    browser.get('http://juliemr.github.io/protractor-demo/').then(function() {
+      console.log("I am the last step to execute.");
+    });
   });
 });
