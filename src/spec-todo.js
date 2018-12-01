d1dec2e7075fb90fc8c6924050417778b8ca8561
Jon <jmkeiste@live.com>
2018-12-01 14:10:01 -0500

    Added test on demo site

--- src/spec-todo.js
+++ src/spec-todo.js
@@ -6,11 +6,14 @@
 //second parameter - function (function will have all test steps)
 
 describe('Protractor Demo App', function() {
-  it('Open AngularJS website', function() {
+  it('Open AngularJS demo website', function() {
     //Protractor code goes here
-    browser.get('https://angularjs.org');
-    browser.get('http://juliemr.github.io/protractor-demo/').then(function() {
-      console.log("I am the last step to execute.");
+    browser.get('http://juliemr.github.io/protractor-demo/');
+    element(by.model("first")).sendKeys("3");
+    element(by.model("second")).sendKeys("5");
+    element(by.id("gobutton")).click();
+    element(by.css("h2[class='ng-binding']")).getText().then(function(text) {
+      console.log(text);
     });
   });
 });
