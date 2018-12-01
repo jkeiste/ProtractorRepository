9eac993303fcb633efa0f940c7ae482a94bb598a
Jon <jmkeiste@live.com>
2018-12-01 18:16:19 -0500

    Refactored calculations into functions

--- src/spec-repeater.js
+++ src/spec-repeater.js
@@ -1,20 +1,39 @@
 describe('Chain locators demo', function() {
-    it('Outputs result of single addition calculation using repeater locator method', function() {
+    it('Outputs result of single addition calculation', function() {
         browser.get('http://juliemr.github.io/protractor-demo/');
-        element(by.model("first")).sendKeys("3");
-        element(by.model("second")).sendKeys("5");
-        element(by.id("gobutton")).click();
+        Add(3, 5);
         element(by.repeater("result in memory")).element(by.css("td:nth-child(3)")).getText().then(function(text) {
             console.log(text);
         });
     });
 
-    it('Outputs result of single multiplication calculation by chaining locator methods', function() {
+    it('Outputs result of single division calculation', function() {
         browser.get('http://juliemr.github.io/protractor-demo/');
-        element(by.model("first")).sendKeys("3");
-        element(by.model("operator")).element(by.css("option:nth-child(4)")).click();
-        element(by.model("second")).sendKeys("5");
-        element(by.id("gobutton")).click();
+        Divide(15, 5);
+        element(by.repeater("result in memory")).element(by.css("td:nth-child(3)")).getText().then(function(text) {
+            console.log(text);
+        });
+    });
+
+    it('Outputs result of single modulus calculation', function() {
+        browser.get('http://juliemr.github.io/protractor-demo/');
+        Modulus(10, 5);
+        element(by.repeater("result in memory")).element(by.css("td:nth-child(3)")).getText().then(function(text) {
+            console.log(text);
+        });
+    });
+
+    it('Outputs result of single multiplication calculation', function() {
+        browser.get('http://juliemr.github.io/protractor-demo/');
+        Multiply(3, 7);
+        element(by.repeater("result in memory")).element(by.css("td:nth-child(3)")).getText().then(function(text) {
+            console.log(text);
+        });
+    });
+
+    it('Outputs result of single subtraction calculation', function() {
+        browser.get('http://juliemr.github.io/protractor-demo/');
+        Subtract(20, 5);
         element(by.repeater("result in memory")).element(by.css("td:nth-child(3)")).getText().then(function(text) {
             console.log(text);
         });
@@ -22,12 +41,10 @@ describe('Chain locators demo', function() {
     
     it('Outputs result of each calculation using repeater locator method', function() {
         browser.get('http://juliemr.github.io/protractor-demo/');
-        element(by.model("first")).sendKeys("2");
-        element(by.model("second")).sendKeys("5");
-        element(by.id("gobutton")).click();
-        element(by.model("first")).sendKeys("4");
-        element(by.model("second")).sendKeys("4");
-        element(by.id("gobutton")).click();
+        Add(5, 5);
+        Subtract(15, 2);
+        Multiply(2, 6);
+        Divide(15, 3);
         element.all(by.repeater("result in memory")).each(function(item) {
             item.element(by.css("td:nth-child(3)")).getText().then(function(text) {
                 console.log(text);
@@ -37,18 +54,48 @@ describe('Chain locators demo', function() {
 
     it('Outputs result of second calculation only using repeater locator method', function() {
         browser.get('http://juliemr.github.io/protractor-demo/');
-        element(by.model("first")).sendKeys("2");
-        element(by.model("second")).sendKeys("5");
-        element(by.id("gobutton")).click();
-        element(by.model("first")).sendKeys("4");
-        element(by.model("second")).sendKeys("4");
-        element(by.id("gobutton")).click();
-        element(by.model("first")).sendKeys("3");
-        element(by.model("second")).sendKeys("3");
-        element(by.id("gobutton")).click();
+        Add(2, 5);
+        Subtract(7, 3);
+        Multiply(6, 6);
         var results = element.all(by.repeater("result in memory"));
         results.get(1).element(by.css("td:nth-child(3)")).getText().then(function(text) {
                 console.log(text);
         });
     });
+
+    //BEGIN HELPER METHOD SECTION:
+    function Add(a, b) {
+        element(by.model("first")).sendKeys(a);
+        element(by.model("operator")).element(by.css("option:nth-child(1)")).click();
+        element(by.model("second")).sendKeys(b);
+        element(by.id("gobutton")).click();
+    };
+
+    function Divide(a, b) {
+        element(by.model("first")).sendKeys(a);
+        element(by.model("operator")).element(by.css("option:nth-child(2)")).click();
+        element(by.model("second")).sendKeys(b);
+        element(by.id("gobutton")).click();
+    };
+
+    function Modulus(a, b) {
+        element(by.model("first")).sendKeys(a);
+        element(by.model("operator")).element(by.css("option:nth-child(3)")).click();
+        element(by.model("second")).sendKeys(b);
+        element(by.id("gobutton")).click();
+    };
+
+    function Multiply(a, b) {
+        element(by.model("first")).sendKeys(a);
+        element(by.model("operator")).element(by.css("option:nth-child(4)")).click();
+        element(by.model("second")).sendKeys(b);
+        element(by.id("gobutton")).click();
+    };
+
+    function Subtract(a, b) {
+        element(by.model("first")).sendKeys(a);
+        element(by.model("operator")).element(by.css("option:nth-child(5)")).click();
+        element(by.model("second")).sendKeys(b);
+        element(by.id("gobutton")).click();
+    };
 });
