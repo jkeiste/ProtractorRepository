906d9af64bed87ea88a54c042e1a33b36dafb987
Jon <jmkeiste@live.com>
2018-12-01 19:07:41 -0500

    Refactored tests and added assertions

--- src/spec-repeater.js
+++ src/spec-repeater.js
@@ -1,50 +1,46 @@
 describe('Chain locators demo', function() {
-    it('Outputs result of single addition calculation', function() {
-        browser.get('http://juliemr.github.io/protractor-demo/');
+    it('Should verify the result of a single addition calculation', function() {
+        GoToDemoSite();
         Add(3, 5);
-        element(by.repeater("result in memory")).element(by.css("td:nth-child(3)")).getText().then(function(text) {
-            console.log(text);
-        });
+        var result = element(by.repeater("result in memory")).element(by.css("td:nth-child(3)")).getText();
+        expect(result).toBe("8");
     });
 
-    it('Outputs result of single division calculation', function() {
-        browser.get('http://juliemr.github.io/protractor-demo/');
+    it('Should verify the result of a single division calculation', function() {
+        GoToDemoSite();
         Divide(15, 5);
-        element(by.repeater("result in memory")).element(by.css("td:nth-child(3)")).getText().then(function(text) {
-            console.log(text);
-        });
+        var result = element(by.repeater("result in memory")).element(by.css("td:nth-child(3)")).getText();
+        expect(result).toBe("3");
     });
 
-    it('Outputs result of single modulus calculation', function() {
-        browser.get('http://juliemr.github.io/protractor-demo/');
+    it('Should verify the result of a single modulus calculation', function() {
+        GoToDemoSite();
         Modulus(10, 5);
-        element(by.repeater("result in memory")).element(by.css("td:nth-child(3)")).getText().then(function(text) {
-            console.log(text);
-        });
+        var result = element(by.repeater("result in memory")).element(by.css("td:nth-child(3)")).getText();
+        expect(result).toBe("0");
     });
 
-    it('Outputs result of single multiplication calculation', function() {
-        browser.get('http://juliemr.github.io/protractor-demo/');
+    it('Should verify the result of a single multiplication calculation', function() {
+        GoToDemoSite();
         Multiply(3, 7);
-        element(by.repeater("result in memory")).element(by.css("td:nth-child(3)")).getText().then(function(text) {
-            console.log(text);
-        });
+        var result = element(by.repeater("result in memory")).element(by.css("td:nth-child(3)")).getText();
+        expect(result).toBe("21");
     });
 
-    it('Outputs result of single subtraction calculation', function() {
-        browser.get('http://juliemr.github.io/protractor-demo/');
+    it('Should verify the result of a single subtraction calculation', function() {
+        GoToDemoSite();
         Subtract(20, 5);
-        element(by.repeater("result in memory")).element(by.css("td:nth-child(3)")).getText().then(function(text) {
-            console.log(text);
-        });
+        var result = element(by.repeater("result in memory")).element(by.css("td:nth-child(3)")).getText();
+        expect(result).toBe("15");
     });
     
-    it('Outputs result of each calculation using repeater locator method', function() {
-        browser.get('http://juliemr.github.io/protractor-demo/');
+    it('Should verify calculations are listed in a table and output the results of each', function() {
+        GoToDemoSite();
         Add(5, 5);
         Subtract(15, 2);
         Multiply(2, 6);
         Divide(15, 3);
+        expect(element.all(by.repeater("result in memory")).count()).toEqual(4);
         element.all(by.repeater("result in memory")).each(function(item) {
             item.element(by.css("td:nth-child(3)")).getText().then(function(text) {
                 console.log(text);
@@ -52,18 +48,23 @@ describe('Chain locators demo', function() {
         });
     });
 
-    it('Outputs result of second calculation only using repeater locator method', function() {
-        browser.get('http://juliemr.github.io/protractor-demo/');
+    it('Should verify calculations are listed in a table and output the second result', function() {
+        GoToDemoSite();
         Add(2, 5);
         Subtract(7, 3);
         Multiply(6, 6);
         var results = element.all(by.repeater("result in memory"));
+        expect(results.count()).toEqual(3);        
         results.get(1).element(by.css("td:nth-child(3)")).getText().then(function(text) {
                 console.log(text);
         });
     });
 
-    //BEGIN HELPER METHOD SECTION:
+    //BEGIN HELPER FUNCTIONS SECTION:
+    function GoToDemoSite() {
+        browser.get('http://juliemr.github.io/protractor-demo/');
+    };
+
     function Add(a, b) {
         element(by.model("first")).sendKeys(a);
         element(by.model("operator")).element(by.css("option:nth-child(1)")).click();
